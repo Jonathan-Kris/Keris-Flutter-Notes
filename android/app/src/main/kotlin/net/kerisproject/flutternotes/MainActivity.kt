7735957005ae938cb1768e0624e17258b2ef3a1e
EmperorJK <kristantojonathan@gmail.com>
2023-07-12 01:11:17 +0700

    feat: Initialize Project for ios & android

---
+++ android/app/src/main/kotlin/net/kerisproject/flutternotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package net.kerisproject.flutternotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutternotes/views/login_view.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Keris Project Flutter Notes',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        useMaterial3: true,
+      ),
+      home: const LoginView(),
+    );
+  }
+}
