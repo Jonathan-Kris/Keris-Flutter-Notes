f83bc868b7fa3c024e429d5137096b7c0b686c8f
EmperorJK <kristantojonathan@gmail.com>
2023-07-17 01:41:08 +0700

    feat: create custom showErrorDialog v2 using AlertDialog

--- lib/widgets/custom_dialog.dart
+++ lib/widgets/custom_dialog.dart
@@ -1,45 +1,92 @@
 import 'package:flutter/material.dart';
 
-void showErrorDialog({required BuildContext context, String? text}) {
-    showDialog(context: context, builder:(context) {
-      return Dialog(
-        alignment: Alignment.center, 
-        child: SizedBox(height: 100.0,
-        width: 80.0, 
-          child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(Icons.error, color: Colors.red),
-                  Text(
-                    text ?? "An error has occured",
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )))
+Future<void> showErrorDialog(
+    {required BuildContext context, String? text}) async {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+            alignment: Alignment.center,
+            child: SizedBox(
+                height: 100.0,
+                width: 80.0,
+                child: Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.error, color: Colors.red),
+                    Text(
+                      text ?? "An error has occured",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ))));
+      });
+}
+
+Future<void> showErrorDialogVandad(
+    {required BuildContext context, required String text}) async {
+  showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("An error has occured"),
+          content: Text(text),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("OK")),
+          ],
         );
       });
 }
 
 void showSuccessDialog({required BuildContext context, String? text}) {
-    showDialog(context: context, builder:(context) {
-      return Dialog(
-        alignment: Alignment.center, 
-        child: SizedBox(height: 100.0,
-        width: 80.0, 
-          child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const Icon(Icons.check_circle, color: Colors.green),
-                  Text(
-                    text ?? "Success",
-                    textAlign: TextAlign.center,
-                  ),
-                ],
-              )))
-        );
+  showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+            alignment: Alignment.center,
+            child: SizedBox(
+                height: 100.0,
+                width: 80.0,
+                child: Center(
+                    child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Icon(Icons.check_circle, color: Colors.green),
+                    Text(
+                      text ?? "Success",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ))));
       });
+}
+
+Future<bool> showLogoutDialog(BuildContext context) {
+  return showDialog<bool>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Sign Out"),
+          content: const Text("Are you sure you want to log out?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(false);
+                },
+                child: const Text("Cancel")),
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop(true);
+                },
+                child: const Text("Log out"))
+          ],
+        );
+      }).then((value) => value ?? false);
 }
